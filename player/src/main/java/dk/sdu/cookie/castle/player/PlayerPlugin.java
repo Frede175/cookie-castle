90e8b61b5495782500c707b08a25982cbc70645f
Kristian <kkwowm@gmail.com>
2019-03-20 11:47:09 +0100

    Added createplayer method for drawing

--- player/src/main/java/dk/sdu/cookie/castle/player/PlayerPlugin.java
+++ player/src/main/java/dk/sdu/cookie/castle/player/PlayerPlugin.java
@@ -1,17 +1,48 @@
 package dk.sdu.cookie.castle.player;
 
+import dk.sdu.cookie.castle.common.data.Entity;
+import dk.sdu.cookie.castle.common.data.Entityparts.LifePart;
+import dk.sdu.cookie.castle.common.data.Entityparts.MovingPart;
+import dk.sdu.cookie.castle.common.data.Entityparts.PositionPart;
 import dk.sdu.cookie.castle.common.data.GameData;
 import dk.sdu.cookie.castle.common.data.World;
 import dk.sdu.cookie.castle.common.services.IGamePluginService;
 
+import java.util.UUID;
+
 public class PlayerPlugin implements IGamePluginService {
-    private Player player;
+    private Entity player;
 
     @Override
     public void start(GameData gameData, World world) {
-        player = new Player();
+        player = createPlayer(gameData);
         world.addEntity(player);
         System.out.println("Started player");
+    }
+
+    private Entity createPlayer(GameData gameData) {
+
+        float deacceleration = 10;
+        float acceleration = 200;
+        float maxSpeed = 300;
+        float rotationSpeed = 5;
+        float x = gameData.getDisplayWidth() / 2;
+        float y = gameData.getDisplayHeight() / 2;
+        float radians = 3.1415f / 2;
+
+        float[] colour = new float[4];
+        colour[0] = 1.0f;
+        colour[1] = 1.0f;
+        colour[2] = 1.0f;
+        colour[3] = 1.0f;
+
+        Entity playerShip = new Player();
+        playerShip.setRadius(8);
+        playerShip.add(new MovingPart(maxSpeed));
+        playerShip.add(new PositionPart(x, y));
+        playerShip.add(new LifePart(1,1,1,1));
+
+        return playerShip;
     }
 
     @Override
