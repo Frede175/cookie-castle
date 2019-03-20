797f610b5c5f677be0724b35cf90e7ebc27fabaa
Lars Jørgensen <lars.b.jn@gmail.com>
2019-03-20 12:38:13 +0100

    Started on enemy plugin
    * Started on enemy plugin

--- enemy/src/main/java/dk/sdu/cookie/castle/enemy/EnemyPlugin.java
+++ enemy/src/main/java/dk/sdu/cookie/castle/enemy/EnemyPlugin.java
@@ -1,23 +1,50 @@
 package dk.sdu.cookie.castle.enemy;
 
+import dk.sdu.cookie.castle.common.data.Entity;
+import dk.sdu.cookie.castle.common.data.Entityparts.LifePart;
+import dk.sdu.cookie.castle.common.data.Entityparts.MovingPart;
+import dk.sdu.cookie.castle.common.data.Entityparts.PositionPart;
 import dk.sdu.cookie.castle.common.data.GameData;
 import dk.sdu.cookie.castle.common.data.World;
 import dk.sdu.cookie.castle.common.services.IGamePluginService;
 
 public class EnemyPlugin implements IGamePluginService {
 
-    private Enemy enemy;
+    private Entity enemy;
 
     @Override
     public void start(GameData gameData, World world) {
-        enemy = new Enemy();
+        enemy = createPlayer(gameData);
         world.addEntity(enemy);
-        System.out.println("Enemy started");
+    }
+
+    private Entity createPlayer(GameData gameData) {
+
+        float deacceleration = 10;
+        float acceleration = 200;
+        float maxSpeed = 300;
+        float rotationSpeed = 5;
+        float x = gameData.getDisplayWidth() / 2;
+        float y = gameData.getDisplayHeight() / 2;
+        float radians = 3.1415f / 2;
+
+        float[] colour = new float[4];
+        colour[0] = 1.0f;
+        colour[1] = 1.0f;
+        colour[2] = 1.0f;
+        colour[3] = 1.0f;
+
+        Entity playerShip = new Enemy();
+        playerShip.setRadius(8);
+        playerShip.add(new MovingPart(maxSpeed));
+        playerShip.add(new PositionPart(x, y));
+        playerShip.add(new LifePart(1,1,1,1));
+
+        return playerShip;
     }
 
     @Override
     public void stop(GameData gameData, World world) {
         world.removeEntity(enemy);
-        System.out.println("Enemy stopped");
     }
 }
