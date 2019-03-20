6f87b8ab5cd89cc9d76d1f9030f45d8377f25fe6
Kristian <kkwowm@gmail.com>
2019-03-20 14:09:52 +0100

    Fixed movingpart

--- common/src/main/java/dk/sdu/cookie/castle/common/data/Entityparts/MovingPart.java
+++ common/src/main/java/dk/sdu/cookie/castle/common/data/Entityparts/MovingPart.java
@@ -5,6 +5,9 @@ import dk.sdu.cookie.castle.common.data.Buff;
 import dk.sdu.cookie.castle.common.data.Entity;
 import dk.sdu.cookie.castle.common.data.GameData;
 
+import static java.lang.Math.abs;
+import static java.lang.StrictMath.sqrt;
+
 public class MovingPart implements EntityPart {
     private float speed;
     private boolean left, right, up, down;
@@ -39,6 +42,8 @@ public class MovingPart implements EntityPart {
         InventoryPart inventoryPart = entity.getPart(InventoryPart.class);
         float x = positionPart.getX();
         float y = positionPart.getY();
+        float deltaX = 0;
+        float deltaY = 0;
         float buffedSpeed = speed;
         if(inventoryPart != null) {
             for (ItemPart itemPart : inventoryPart.getItemParts()) {
@@ -50,19 +55,30 @@ public class MovingPart implements EntityPart {
         }
 
         if (left) {
-            x -= buffedSpeed;
+            deltaX -= buffedSpeed;
         }
 
         if (right) {
-            x += buffedSpeed;
+            deltaX += buffedSpeed;
         }
 
         if (up) {
-            y += buffedSpeed;
+            deltaY += buffedSpeed;
         }
 
         if (down) {
-            y -= buffedSpeed;
+            deltaY -= buffedSpeed;
+        }
+
+        if((abs(deltaX) + abs(deltaY)) > buffedSpeed) {
+            deltaX = (deltaX/ (float) sqrt((deltaX*deltaX) + (deltaY*deltaY))) * buffedSpeed;
+            deltaY = (deltaY/ (float) sqrt((deltaX*deltaX) + (deltaY*deltaY))) * buffedSpeed;
+            x += deltaX;
+            y += deltaY;
+
+        }   else {
+            x += deltaX;
+            y += deltaY;
         }
 
         positionPart.setPosition(x, y);
