75b81cae8f2ba4689765ea23687434ef5b1c8df5
Niemeijeren <niemeijeren@gmail.com>
2019-03-21 10:49:43 +0100

    EntityTypes

---
+++ common/src/main/java/dk/sdu/cookie/castle/common/data/EntityType.java
@@ -0,0 +1,13 @@
+package dk.sdu.cookie.castle.common.data;
+
+public enum EntityType {
+    PLAYER,
+    ENEMY,
+    STATIC_OBSTACLE,
+    REMOVABLE_OBSTACLE,
+    PLAYER_BULLET,
+    ENEMY_BULLET,
+    WALL,
+    DOOR,
+    ITEM,
+}
