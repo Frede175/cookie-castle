6fb7e0d99bc2cc8d736260ca15b778ccd4dbc531
Kristian <kkwowm@gmail.com>
2019-03-26 10:07:34 +0100

    Added itemplugin and itemprocessing methods for drawing an item

--- item/src/main/java/dk/sdu/cookie/castle/item/ItemPlugin.java
+++ item/src/main/java/dk/sdu/cookie/castle/item/ItemPlugin.java
@@ -1,16 +1,20 @@
 package dk.sdu.cookie.castle.item;
 
+import dk.sdu.cookie.castle.common.data.Entity;
+import dk.sdu.cookie.castle.common.data.Entityparts.CollisionPart;
+import dk.sdu.cookie.castle.common.data.Entityparts.LifePart;
+import dk.sdu.cookie.castle.common.data.Entityparts.PositionPart;
 import dk.sdu.cookie.castle.common.data.GameData;
 import dk.sdu.cookie.castle.common.data.World;
 import dk.sdu.cookie.castle.common.services.IGamePluginService;
 
 public class ItemPlugin implements IGamePluginService {
 
-    private Item item;
+    private Entity item;
 
     @Override
     public void start(GameData gameData, World world) {
-        item = new Item();
+        item = createItem(gameData);
         world.addEntity(item);
         System.out.println("Jeff");
     }
@@ -20,4 +24,24 @@ public class ItemPlugin implements IGamePluginService {
         world.removeEntity(item);
         System.out.println("No Jeff");
     }
+
+    private Entity createItem(GameData gameData) {
+        float x = gameData.getDisplayWidth() / 2 + 200;
+        float y = gameData.getDisplayHeight() / 2 + 150;
+
+        float[] shapex = new float[6];
+        float[] shapey = new float[6];
+
+
+        Entity item = new Item();
+        item.add(new PositionPart(x, y));
+        item.add(new LifePart(1, 1,1, 30));
+        item.setRadius(10);
+        item.add(new CollisionPart());
+        item.setShapeX(shapex);
+        item.setShapeY(shapey);
+
+        return item;
+    }
+
 }
